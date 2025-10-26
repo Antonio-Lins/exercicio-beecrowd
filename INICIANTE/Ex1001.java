4cecff3f14eea9cd965113112e255579b2f9c46b
Antonio-Lins <lins240608@gmail.com>
2025-10-25 22:43:49 -0300

    Ex1001 beecrowd Java solution

---
+++ INICIANTE/Ex1001.java
@@ -0,0 +1,18 @@
+package INICIANTE;
+
+import java.util.Scanner;
+
+public class Ex1001 {
+    public static void main(String[]args){
+        int Num1;
+        Scanner sc = new Scanner(System.in);
+        Num1 = sc.nextInt();
+        int Num2;
+        Num2 = sc.nextInt();
+        int Soma = Num1 + Num2;
+        System.out.println("X = " + Soma);
+
+
+    }
+    
+}
